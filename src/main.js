70cc5ca7a4155f3e22daa45036d7dba3094fd329
Samuele Deriu <samuele.deriu.dev@gmail.com>
2026-09-09 23:10:29 +0200

    feat: implement fizzbuzz core logic and render fizzbuzz result in the DOM

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-import "./style.css";
-import heroImg from "./assets/hero.png";
-import javascriptLogo from "./assets/javascript.svg";
-import viteLogo from "./assets/vite.svg";
-import { setupCounter } from "./counter.js";
+export function fizzbuzz(n) {
+  const res = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 == 0) res.push("FizzBuzz");
+    else if (i % 5 == 0) res.push("Buzz");
+    else if (i % 3 == 0) res.push("Fizz");
+    else res.push(String(i));
+  }
+  return res;
+}
 
+const result = fizzbuzz(100);
 document.querySelector("#app").innerHTML = `
+<h1>FizzBuzz</h1>
+<div>
+${result.map((item) => `<div>${item}</div>`).join("")}
+</div>
 `;
-
-setupCounter(document.querySelector("#counter"));
